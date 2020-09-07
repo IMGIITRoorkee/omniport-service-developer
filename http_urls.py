4a6eeb0cb9ce1f1cfec729dabfd994109342a4c3
gauransh7 <gauransh7dingwani@gmail.com>
2020-09-08 00:50:57 +0530

    added path to hidden details view

--- http_urls.py
+++ http_urls.py
@@ -1,7 +1,8 @@
 from django.urls import include, path
+from django.conf.urls import url
 from rest_framework import routers
 
-from developer.views.application import ApplicationViewSet
+from developer.views.application import ApplicationViewSet, ApplicationHiddenDetailView
 
 app_name = 'developer'
 
@@ -10,4 +11,5 @@ router.register(r'application', ApplicationViewSet, basename='application')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('application/<int:id>/secret_data/',ApplicationHiddenDetailView.as_view(),name="hidden-detail"),
 ]
