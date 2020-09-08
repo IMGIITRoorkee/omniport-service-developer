e1dce5baee7889e35c495db05475de0477679e02
gauransh7 <gauransh7dingwani@gmail.com>
2020-09-08 18:43:10 +0530

    Remove needless imports & id from url path, improve formatting

--- http_urls.py
+++ http_urls.py
@@ -1,5 +1,5 @@
 from django.urls import include, path
-from django.conf.urls import url
+
 from rest_framework import routers
 
 from developer.views.application import ApplicationViewSet, ApplicationHiddenDetailView
@@ -10,6 +10,6 @@ router = routers.SimpleRouter()
 router.register(r'application', ApplicationViewSet, basename='application')
 
 urlpatterns = [
+    path('application/secret_data/', ApplicationHiddenDetailView.as_view(), name="hidden-detail"),
     path('', include(router.urls)),
-    path('application/<int:id>/secret_data/',ApplicationHiddenDetailView.as_view(),name="hidden-detail"),
 ]
